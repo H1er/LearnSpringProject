45349f8d9558d81bd5b7438477002f252ad0628f
h1er <jesusgarciacarrasco99@gmail.com>
2023-10-21 00:46:22 +0200

    owner data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-INSERT INTO "animal" (id, age, name, race) VALUES
-                                             (1,12,'nova','gato'),
-                                             (2,5,'calcetines','gato'),
-                                             (3,1,'boby','perro'),
-                                             (4,7,'perry','ornitorrinco');
+INSERT INTO "owner" ("id","name","age","address") VALUES
+                                                      (1,'Mike',19,'C/Street, 2'),
+                                                      (2,'Anthony',32,'C/Road, 19'),
+                                                      (3,'phineas',14,'C/Candace, 22');
+
+INSERT INTO "animal" (id, age, name, race, owner_id) VALUES
+                                             (1,12,'nova','gato',2),
+                                             (2,5,'calcetines','gato',1),
+                                             (3,1,'boby','perro',1),
+                                             (4,7,'perry','ornitorrinco',3);
+
