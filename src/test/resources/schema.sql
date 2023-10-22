8bf93a9879329baccc2b42344c791a1c7b07045c
h1er <jesusgarciacarrasco99@gmail.com>
2023-10-22 19:48:15 +0200

    test database schema script added

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS "animal";
+DROP TABLE IF EXISTS "owner";
+
+CREATE TABLE "owner"
+(
+    "id"      INTEGER PRIMARY KEY,
+    "name"    VARCHAR(100),
+    "age"     INTEGER,
+    "address" VARCHAR(500)
+);
+
+CREATE TABLE "animal"(
+    "id"   INTEGER PRIMARY KEY,
+    "age"  INTEGER,
+    "name" VARCHAR(50),
+    "race" VARCHAR(50),
+    "owner_id" INTEGER,
+    CONSTRAINT "owner_id_fk" FOREIGN KEY (owner_id) REFERENCES owner(id)
+);
