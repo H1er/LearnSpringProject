6300ea75275d8efbde713e7f8ecf73109a2b4850
h1er <jesusgarciacarrasco99@gmail.com>
2023-11-07 12:46:57 +0100

    jpa annotations added to become entities

--- src/main/java/com/example/demo/domain/Animal.java
+++ src/main/java/com/example/demo/domain/Animal.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +10,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
+@Entity
+@Table(name="animal")
 public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "animal_id_seq")
     private Long id;
+
     private Long age;
     private String name;
     private String race;
-    private Long owner_id;
+
+    @ManyToOne(cascade = CascadeType.ALL) //all changes made to the owner from here will be stored in db
+    @JoinColumn(name = "owner_id") //name of the column used in db to reference
+    private Owner owner;
 }
--- src/main/java/com/example/demo/domain/Owner.java
+++ src/main/java/com/example/demo/domain/Owner.java
@@ -1,17 +1,27 @@
 package com.example.demo.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 
+//lombok annotations
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
+//spring data jpa annotations
+@Entity
+@Table(name = "owner") //assigns entity to a table in db
 public class Owner {
+
+    @Id //tabla id field
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "owner_id_seq") //gives value automatically
     private Long id;
+
     private String name;
     private Long age;
     private String address;
